8a489420047cec0bedfd443bd0a47cf3a89d3ef4
Breadcrumbs <william.qiao.yang@gmail.com>
2026-08-22 16:57:18 -0700

    add cards

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,15 @@
 public class Card {
+    public int suit;
+    public int number;
+
+    public Card(int suit, int number) {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Suit: %d, Number: %d", suit, number);
+
+    }
 }
