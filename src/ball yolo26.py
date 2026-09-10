e14589a128e958ee7cbe8f6f95ce6d8c4b853f92
Bloob <acceleration9992@gmail.com>
2026-09-09 17:26:40 -0700

    nuke

--- src/ball yolo26.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO # type: ignore
-
-model = YOLO("yolo26n.yaml")
-results = model.train(data="model/data.yaml", epochs=20, imgsz=512)
--- src/basics.py
+++
@@ -1,8 +0,0 @@
-print("Dj is cool")
-#unlike Java, Python helps increase productivity and reduce the amount of code needed to perform tasks.
-my_variable = "This is a variable in Python"
-your_variable = 1
-your_variablenumbertwo = 2.0
-
-for i in range (5):
-    print(i)
